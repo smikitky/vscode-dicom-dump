d727c8024fc794f387ade17df96fca0c456156b8
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-07-05 15:09:07 +0900

    Implement command framework

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,29 @@
 'use strict';
 import * as vscode from 'vscode';
 
+const schema = 'dicomtags';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('"vscode-dicom" is now active!');
 
   const provider = new DicomContentProvider();
   const registrations = vscode.Disposable.from(
-    vscode.workspace.registerTextDocumentContentProvider('dicomtags', provider)
+    vscode.workspace.registerTextDocumentContentProvider(schema, provider)
   );
 
-  context.subscriptions.push(registrations);
+  const commandRegistration = vscode.commands.registerTextEditorCommand(
+    'dicom.showTags',
+    async editor => {
+      const uri = encodeLocation(editor.document.uri);
+      const doc = await vscode.workspace.openTextDocument(uri);
+      return vscode.window.showTextDocument(doc, (editor.viewColumn || 0) + 1);
+    }
+  );
+
+  context.subscriptions.push(registrations, commandRegistration);
 }
 
 // this method is called when your extension is deactivated
@@ -25,6 +35,18 @@ export function deactivate() {}
  */
 class DicomContentProvider implements vscode.TextDocumentContentProvider {
   public provideTextDocumentContent(url: vscode.Uri): Promise<string> {
-    return Promise.resolve('foobar');
+    return Promise.resolve(url.toString());
   }
+}
+
+let seq = 0;
+
+export function encodeLocation(uri: vscode.Uri): vscode.Uri {
+  const query = JSON.stringify([uri.toString()]);
+  return vscode.Uri.parse(`${schema}:dicom-tag-dump?${query}#${seq++}`);
+}
+
+export function decodeLocation(uri: vscode.Uri): [vscode.Uri] {
+  let [target] = <[string]>JSON.parse(uri.query);
+  return [vscode.Uri.parse(target)];
 }
