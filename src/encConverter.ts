df0a13ca3757abb6453c0d10686994d355c612d3
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-07-08 06:03:14 +0900

    More spec-compliant encode handling

--- src/encConverter.ts
+++ src/encConverter.ts
@@ -5,88 +5,58 @@
  * LT (Long Text), PN (Person Name) and UT (Unlimited Text).
  */
 export interface EncConverter {
-  (input: Buffer, vr?: string): string;
+  (input: Buffer, vr: string): string;
 }
 
-interface AvailableConverter {
-  pattern: RegExp;
-  converter: (charSet: string) => Promise<EncConverter>;
-}
+type Decoder = (input: Buffer) => string;
 
 /**
  * A higher-order function that returns a new function,
- * which in turn returns an EncConverter.
+ * which in turn returns a Decoder.
+ * We use HoF because we want to load external modules as late as possible.
  * @param encoding
  */
-const iconvLite: (
-  encoding: string
-) => (() => Promise<EncConverter>) = encoding => {
+const iconvLite: (encoding: string) => (() => Promise<Decoder>) = encoding => {
   return async () => {
     const iconv = await import('iconv-lite');
     return buffer => iconv.decode(buffer, encoding);
   };
 };
 
-const convertJapanesePn = (buffer: Buffer, charSet: string, jconv: any) => {
-  const map: { [key: string]: (buffer: Buffer) => string } = {
-    'IR 6': b => b.toString('utf8'),
-    'IR 13': b => jconv.decode(b, 'sjis'),
-    'IR 87': b => jconv.decode(b, 'iso-2022-jp')
-  };
-  const charSets = charSet.split('\\').map(s => s.trim());
-  if (charSets[0] === '') charSets[0] = 'ISO 2022 IR 6';
-  const components = buffer.toString('binary').split('=');
-
-  return components
-    .map((component, index) => {
-      const cs =
-        index < charSets.length
-          ? charSets[index]
-          : charSets[charSets.length - 1];
-      const enc = Object.keys(map).find(k => cs.indexOf(k) >= 0) || 'IR 6';
-      return map[enc](Buffer.from(component, 'binary'));
-    })
-    .join('=');
-};
-
-/**
- * Mapping of "specific character set defined terms" and EncConverter.
- */
-const encodingMap: AvailableConverter[] = [
-  {
-    // Japanese ISO-2022-JP (aka "JIS"), including half-width katakana.
-    pattern: /IR\s?(87|13)\b/,
-    converter: async charSet => {
-      // Currently iconv-lite does not support this encoding.
-      // ISO-IR 13 is rarely-used 7-bit hankaku katakana.
-      const csValues = charSet.split('\\').map(s => s.trim());
-      if (csValues[0] === '') csValues[0] = 'ISO-IR 6';
-
-      const jconv = await import('jconv');
-      return (buffer, vr) => {
-        if (vr !== 'PN') return jconv.decode(buffer, 'ISO-2022-JP');
-        return convertJapanesePn(buffer, charSet, jconv);
-      };
-    }
-  },
-  { pattern: /IR\s?100\b/, converter: iconvLite('iso-8859-1') }, // Latin-1
-  { pattern: /IR\s?101\b/, converter: iconvLite('iso-8859-2') }, // Latin-2
-  { pattern: /IR\s?109\b/, converter: iconvLite('iso-8859-3') }, // Latin-3
-  { pattern: /IR\s?110\b/, converter: iconvLite('iso-8859-4') }, // Latin-4
-  { pattern: /IR\s?144/, converter: iconvLite('iso-8859-5') }, // Cyrillic
-  { pattern: /IR\s?127/, converter: iconvLite('iso-8859-6') }, // Arabic
-  { pattern: /IR\s?126/, converter: iconvLite('iso-8859-7') }, // Greek
-  { pattern: /IR\s?138/, converter: iconvLite('iso-8859-8') }, // Hebrew
-  { pattern: /IR\s?148/, converter: iconvLite('iso-8859-9') }, // Latin-5
-  { pattern: /IR\s?192\b/, converter: iconvLite('utf-8') }, // UTF-8
-  { pattern: /GB18030/, converter: iconvLite('gb18030') }, // Chinese
-  { pattern: /GBK/, converter: iconvLite('gbk') }, // Chinese
-  { pattern: /IR\s?166\b/, converter: iconvLite('tis620') } // Thai
-  // Korean (ISO IR 149) needs pure-JS "ISO-2022-KR" solution...
-];
-
 // cf:
 // https://github.com/InsightSoftwareConsortium/DCMTK/blob/master/dcmdata/libsrc/dcspchrs.cc#L168
+
+const encMap: { [key: string]: () => Promise<Decoder> } = {
+  'IR 6': async () => b => b.toString('utf8'), // ASCII (but utf8 is compatible)
+  'IR 13': iconvLite('sjis'), // Japanese half-width katakana (sjis is compatible)
+  'IR 87': async () => {
+    // Japanese JIS kanji
+    const jconv = await import('jconv');
+    return b => jconv.decode(b, 'iso-2022-jp');
+  },
+  'IR 149': async () => {
+    // Korean
+    const iconv = await import('iconv-lite');
+    // HACK!: FIXME!: We should use a pure-JS ISO-2022-KR implementation,
+    // but here we just ignore the escape sequence.
+    return b => iconv.decode(b, 'euc-kr').replace(/\x1b\x24\x29\x43/g, '');
+  },
+  'IR 100': iconvLite('iso-8859-1'), // Latin-1
+  'IR 101': iconvLite('iso-8859-2'), // Latin-2
+  'IR 109': iconvLite('iso-8859-3'), // Latin-3
+  'IR 110': iconvLite('iso-8859-4'), // Latin-4
+  'IR 144': iconvLite('iso-8859-5'), // Cyrillic
+  'IR 127': iconvLite('iso-8859-6'), // Arabic
+  'IR 126': iconvLite('iso-8859-7'), // Greek
+  'IR 138': iconvLite('iso-8859-8'), // Hebrew
+  'IR 148': iconvLite('iso-8859-9'), // Latin-5
+  'IR 192': iconvLite('utf-8'), // UTF-8
+  GB18030: iconvLite('gb18030'), // Chinese
+  GBK: iconvLite('gbk'), // Chinese
+  'IR 166': iconvLite('tis620') // Thai
+};
+
+const createdDecoders = new Map<string, Decoder>();
 
 /**
  * Examines the value of SpecificCharacterSet (0008,0005) and
@@ -96,11 +66,41 @@ const encodingMap: AvailableConverter[] = [
 export async function createEncConverter(
   charSet: string
 ): Promise<EncConverter | undefined> {
-  for (let item of encodingMap) {
-    if (item.pattern.test(charSet)) {
-      return await item.converter(charSet);
-    }
+  // SpecificCharacterSet may have more than one value, delimited by '\'.
+  // If the first value is omitted, it becomes default repertoire, i.e., ASCII.
+  const charSets = charSet.split('\\').map(s => s.trim());
+  if (charSets[0] === '') charSets[0] = 'IR 6'; // ASCII
+  const decoders: EncConverter[] = [];
+
+  // Now asynchronously create decoders that corresponds to each value.
+  // External modules required for conversion will be lazily-loaded here.
+  for (let cs of charSets) {
+    const enc = Object.keys(encMap).find(k => cs.endsWith(k));
+    if (!enc) return undefined;
+    const decoder = createdDecoders.get(enc) || (await encMap[enc]());
+    createdDecoders.set(enc, decoder);
+    decoders.push(decoder);
   }
-  // None matched, meaning some unknown encoding has been specified
-  return undefined;
+
+  // Creates the new EncConverter and returns it.
+  return (buffer: Buffer, vr: string) => {
+    if (vr !== 'PN') {
+      return decoders[0](buffer, vr);
+    } else {
+      // If VR is 'PN', we need to separately decode each
+      // component delimited by '='.
+      const components = buffer
+        .toString('binary')
+        .split('=')
+        .map(s => Buffer.from(s, 'binary'));
+      const decodedComponents = components.map((component, index) => {
+        const decoder =
+          index < decoders.length
+            ? decoders[index]
+            : decoders[charSets.length - 1];
+        return decoder(component, vr);
+      });
+      return decodedComponents.join('=');
+    }
+  };
 }
